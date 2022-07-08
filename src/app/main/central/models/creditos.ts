d0347dcc8fde1319834f48790a6996d02ad33d62
Abraham Vallejos <abraham-rat@outlook.es>
2022-07-08 10:52:21 -0500

    Fixed [Abraham Vallejos]: Fixed view solicitudes-creditos.component.html added logo and other attributes to the table

--- src/app/main/central/models/creditos.ts
+++ src/app/main/central/models/creditos.ts
@@ -1,9 +1,27 @@
-export class ActualizarCredito {
+export class SolicitudesCreditos {
     id: string;
-    reporteBuroCredito: string;
-    calificacionBuro: string;
-    tomarSolicitud: string;
-    buroCredito: string;
-    identificacion: string;
+    numero?: string;
+    monto?: string;
+    plazo?: string;
+    aceptaTerminos?: string;
+    estado?: string;
+    user_id?: string;
+    empresaComercial_id?: string;
+    empresaIfis_id?: string;
+    reporteBuro?: string;
+    calificacionBuro?: string;
     buroValido?: string;
+    identificacion?: string;
+    ruc?: string;
+    rolesPago?: string;
+    panillaIESS?: string;
+    created_at?: string;
+    updated_at?: string;
+    state?: string;
+    tomarSolicitud?: string;
+    tipoCredito?: string;
+    concepto?: string;
+    fechaAprobacion?: string;
+    documentoAprobacion?: string;
+
 }
